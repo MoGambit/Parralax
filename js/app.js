1cf2bf26d2bf80295a9bd846cde1d065daa43771
MoGambit <68572892+MoGambit@users.noreply.github.com>
2023-06-20 23:31:23 +0530

    animation is done , only need to fix  scaling

--- js/app.js
+++ js/app.js
@@ -1,25 +1,91 @@
 const parralax_el = document.querySelectorAll(".parralax");
+const main = document.querySelector("main");
 
 let xValue = 0,
     yValue = 0;
 
+    let rotateDegree = 0;
+
+    function update(cursorPosition) {
+        parralax_el.forEach((el) => {
+            let speedx = el.dataset.speedx;
+            let speedy = el.dataset.speedy;
+            let speedz = el.dataset.speedz;
+            let rotateSpeed = el.dataset.rotation;
+    
+            let isInLeft = 
+            parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
+    
+            let zValue = 
+            (cursorPosition - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
+    
+            el.style.transform = `perspective(2300px) translateZ(${
+                zValue * speedz
+            }px) rotateY(${rotateDegree * rotateSpeed}deg) translateX(calc(-50% + ${
+                -xValue * speedx
+            }px)) translateY(calc(-50% + ${yValue * speedy}px))`;
+        });
+    }
+
+    update(0);
+
 window.addEventListener("mousemove", (e) => {
+    if(timeline.isActive()) return;
+
     xValue = e.clientX - window.innerWidth / 2;
     yValue = e.clientY - window.innerHeight / 2;
 
-    parralax_el.forEach((el) => {
-        let speedx = el.dataset.speedx;
-        let speedy = el.dataset.speedy;
+    rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
 
-        const forTest = document.querySelector(".mountain-2");
-
-        let zValue = e.clientX - parseFloat(getComputedStyle(forTest).left);
-        console.log(zValue);
-
-        el.style.transform = `translateX(calc(-50% + ${
-            -xValue * speedx
-        }px)) translateY(calc(-50% + ${
-            yValue * speedy
-        }px)) perspective(2300px) translateZ(${zValue}px)`;
-    });
+    update(e.clientX);
 });
+
+if(window.innerWidth >= 725) {
+    main.style.maxHeight = `${window.innerWidth * 0.6}px`;
+} else {
+    main.style.maxHeight = `${window.innerWidth * 1.6}px`;
+}
+
+/* GSAP Animation */
+
+let timeline = gsap.timeline();
+
+Array.from(parralax_el)
+.filter((el) => !el.classList.contains("text"))
+.forEach((el) => {
+    timeline.from(
+        el, 
+        {
+            top: `${el.offsetHeight / 2 + +el.dataset.distance}px`,
+            duration: 3.5,
+            ease: "power3.out",
+        },
+        "1"
+    );
+});
+
+timeline.from(".text h1", {
+    y: window.innerHeight - document.querySelector(".text h1").getBoundingClientRect().top + 
+    200,
+    duration: 2,
+},
+"2.5"
+)
+.from(
+    ".text h2",
+    {
+        y: -150,
+        opacity: 0,
+        duration: 1.5,
+    },
+    "3"
+)
+.from(
+    ".hide", 
+{
+    opacity: 0,
+    duration: 1.5,
+},
+"3"
+);
+
